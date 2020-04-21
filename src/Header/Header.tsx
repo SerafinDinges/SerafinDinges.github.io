40323de5ae1962d193de045c07aa98a452126c14
Serafin Dinges <dingesserafin@gmail.com>
2020-04-21 16:45:30 +0100

    update design

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -5,7 +5,9 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-            <h1>someta.xyz</h1>
+                <h1>someta.xyz</h1>
+                <p>A tool to investigate data relating but not exclusive to Covid-19.</p>
+                <p><i>by <a href="http://serafin.tv">Serafin Dinges</a></i></p>
             </header>
         );
     }
