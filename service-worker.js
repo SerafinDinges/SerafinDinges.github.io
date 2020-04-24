34b6f5b21648d99fa39cb63a76b99d7dab3a92dc
Serafin Dinges <dingesserafin@gmail.com>
2020-04-24 13:55:34 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63354b712239029ac873f77ca16c19c5.js"
+  "/precache-manifest.f3565af4691f88b544652816b7d98a31.js"
 );
 
 self.addEventListener('message', (event) => {
